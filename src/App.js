9e4877d678859870e4dd8820869587d2b9ba9c40
Rob Misiorowski <rmisio@users.noreply.github.com>
2019-03-15 16:45:47 -0600

    another commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import IPFS from 'ipfs';
 import './App.css';
 
 class App extends Component {
+  state = {
+
+  }
+
+  componentDidMount() {
+    console.log('i be mounted');
+
+    const node = this.node = new IPFS();
+
+    node.on('ready', async () => {
+      console.log('the node is moo');
+      window.moo = this.node;
+
+      // window.zillow = node.libp2p.handle('/openbazaar/app/1.0.0', (protocol, conn) => {
+      //   console.log('pulling in incoming message');
+      // });
+
+      const nodeId = await node.id();
+      const randomToken = Math.random().toString().slice(0, 6);
+
+      const fileAdded = await node.add({
+        path: 'happy.times',
+        content: Buffer.from(`I am happy. I am good. (${randomToken})`),
+      })
+
+      console.log('Added file:');
+      console.dir(fileAdded);
+
+      const filePublished = await node.name.publish(`/ipfs/${fileAdded[0].hash}`);
+
+      console.log('File published.');
+      console.dir(filePublished);
+
+      const nameResolve = await node.name.resolve(`/ipns/${nodeId.id}/happy.times`);
+
+      console.log('Name resolved. Boom.');
+      console.dir(nameResolve);
+
+      const fileCat = await node.cat(nameResolve.path);
+
+      console.log('catted dat file dog. cat.');
+      console.log(fileCat.toString());
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
